a480ae94b75664b65770d4f800597c983a632b9d
Eduariag <uariagsmart@gmail.com>
2023-03-14 11:58:51 +0300

    2 задачи решены

---
+++ Homework1.py
@@ -0,0 +1,18 @@
+#Задача1 
+#Сумма чисел трехзначного числа
+'''
+n=int(input('enter a three-digit number:  '))
+a=n%10
+b=n%100//10
+c=n//100
+print("the sum of the digits of the number:" , a+b+c)
+'''
+
+#Задача 2 Сколько всего журавликов сделают
+'''
+a=int(input('Количество журавликов Сережи:  '))
+b=a
+c=(a+b)*2
+print(a+b+c)
+'''
+
