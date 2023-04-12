79eba918ecc9859acd39e66052a8e8fc996cbbd3
Eduariag <uariagsmart@gmail.com>
2023-04-12 09:50:38 +0300

    решение1

--- examen.py
+++ examen.py
@@ -1,6 +1,6 @@
 #В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. 
 #Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
-
+'''
 import pandas as pd 
 import numpy as np 
 import random
@@ -18,3 +18,4 @@ data = data.unstack(level=-1, fill_value = 0).astype(int)
 data.columns = data.columns.droplevel()
 data.columns.name = None
 print(data)
+'''
